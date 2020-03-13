5533eae1af792f8ad8d04702d80bebf527311ef3
Rustam @Second_Fry Gubaydullin <secondfry@gmail.com>
2020-03-13 23:30:54 +0300

    WIP

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: oadhesiv <oadhesiv@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/29 14:00:03 by oadhesiv          #+#    #+#             */
-/*   Updated: 2020/03/13 22:11:57 by oadhesiv         ###   ########.fr       */
+/*   Updated: 2020/03/13 23:30:15 by oadhesiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,12 @@ int		main(int argc, char** argv) {
 	t_color *g = color_new(50, 150, 0);
 	t_color *e = color_sub(a, f);
 	t_color *h = color_add(e, g);
+	color_free(a);
+	color_free(f);
+	color_free(g);
+	color_free(e);
+	color_free(h);
+
 	t_color *i = color_mult(h, 0.1);
 
 	// color_print(a); ft_putchar('\n');
@@ -53,34 +59,38 @@ int		main(int argc, char** argv) {
 	// color_print(h); ft_putchar('\n');
 	// color_print(i); ft_putchar('\n');
 
-	t_vertex *b = vertex_new(0, 1, 2, 3);
-	// t_vertex *c = vertex_new(1, 4, 5, 6, 7.0);
-	// t_vertex *d = vertex_new(2, 8, 9, 10, 11.0, i);
-	// t_vertex *z = vertex_new(1, 0, 0, 0, 0.0);
-	// t_vertex *w = vertex_new(1, 1, 1, 1, 0.0);
+	// t_vertex *b = vertex_new(0, 1, 2, 3);
+	t_vertex *c = vertex_new(1, 4, 5, 6, 7.0);
+	t_vertex *d = vertex_new(2, 8, 9, 10, 11.0, i);
+	t_vertex *z = vertex_new(1, 0, 0, 0, 0.0);
+	t_vertex *w = vertex_new(1, 1, 1, 1, 0.0);
+	vertex_free(c);
+	vertex_free(d);
+	vertex_free(z);
+	vertex_free(w);
 
-	vertex_print(b); ft_putchar('\n');
+	// vertex_print(b); ft_putchar('\n');
 	// vertex_print(c); ft_putchar('\n');
 	// vertex_print(d); ft_putchar('\n');
 
 	// t_vector *vz = vector_new(0, b);
-	// t_vector *va = vector_new(1, w, z);
-	// t_vector *vb = vector_new(1, c, d);
+	t_vector *va = vector_new(1, w, z);
+	t_vector *vb = vector_new(1, c, d);
 
-	// // vector_print(va); ft_putchar('\n');
-	// // printf("Magnitude: %f\n", vector_magnitude(va));
+	// vector_print(va); ft_putchar('\n');
+	// printf("Magnitude: %f\n", vector_magnitude(va));
 
-	// // vector_print(vb); ft_putchar('\n');
-	// // printf("Magnitude: %f\n", vector_magnitude(vb));
+	// vector_print(vb); ft_putchar('\n');
+	// printf("Magnitude: %f\n", vector_magnitude(vb));
 
 	// t_vector *vc = vector_normalize(va);
 	// t_vector *vd = vector_normalize(vb);
 
-	// // vector_print(vc); ft_putchar('\n');
-	// // printf("Magnitude: %f\n", vector_magnitude(vc));
+	// vector_print(vc); ft_putchar('\n');
+	// printf("Magnitude: %f\n", vector_magnitude(vc));
 
-	// // vector_print(vd); ft_putchar('\n');
-	// // printf("Magnitude: %f\n", vector_magnitude(vd));
+	// vector_print(vd); ft_putchar('\n');
+	// printf("Magnitude: %f\n", vector_magnitude(vd));
 
 	// t_vector *ve = vector_add(va, vb);
 
@@ -102,8 +112,11 @@ int		main(int argc, char** argv) {
 	// vector_print(vh); ft_putchar('\n');
 	// printf("Magnitude: %f\n", vector_magnitude(vh));
 
-	// double dp = vector_dot_product(va, vb);
-	// printf("%f\n", dp);
+	double dp = vector_dot_product(va, vb);
+	printf("%f\n", dp);
+
+	vector_free(va);
+	vector_free(vb);
 
 	// mlx_loop(mlx);
 
